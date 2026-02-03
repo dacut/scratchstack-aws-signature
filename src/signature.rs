9a521783fd7e6123a275893f59b908df59e6b157
David Cuthbert <dacut@kanga.org>
2026-02-03 11:27:32 -0800

    Factor out HTTP request body handling types

--- src/signature.rs
+++ src/signature.rs
@@ -1,7 +1,7 @@
 use {
     crate::{
-        auth::SigV4AuthenticatorResponse, canonical::CanonicalRequest, constants::*, GetSigningKeyRequest,
-        GetSigningKeyResponse, SignedHeaderRequirements,
+        auth::SigV4AuthenticatorResponse, body::IntoRequestBytes, canonical::CanonicalRequest, constants::*,
+        GetSigningKeyRequest, GetSigningKeyResponse, SignedHeaderRequirements,
     },
     bytes::Bytes,
     chrono::{DateTime, Duration, Utc},
@@ -107,44 +107,6 @@ where
         .await?;
 
     Ok((parts, body, sigv4_response))
-}
-
-/// A trait for converting various body types into a [`Bytes`] object.
-///
-/// This requires reading the entire body into memory.
-pub trait IntoRequestBytes {
-    /// Convert this object into a [`Bytes`] object.
-    fn into_request_bytes(self) -> impl Future<Output = Result<Bytes, BoxError>> + Send + Sync;
-}
-
-/// Convert the unit type `()` into an empty [`Bytes`] object.
-impl IntoRequestBytes for () {
-    /// Convert the unit type `()` into an empty [`Bytes`] object.
-    ///
-    /// This is infalliable.
-    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
-        Ok(Bytes::new())
-    }
-}
-
-/// Convert a `Vec<u8>` into a [`Bytes`] object.
-impl IntoRequestBytes for Vec<u8> {
-    /// Convert a `Vec<u8>` into a [`Bytes`] object.
-    ///
-    /// This is infalliable.
-    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
-        Ok(Bytes::from(self))
-    }
-}
-
-/// Identity transformation: return the [`Bytes`] object as-is.
-impl IntoRequestBytes for Bytes {
-    /// Identity transformation: return the [`Bytes`] object as-is.
-    ///
-    /// This is infalliable.
-    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
-        Ok(self)
-    }
 }
 
 #[cfg(test)]
