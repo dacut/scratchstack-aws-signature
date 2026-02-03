b40cf41c0034d8fc4b962b99e8c03d51f90304ca
David Cuthbert <dacut@kanga.org>
2026-02-03 11:27:32 -0800

    Factor out HTTP request body handling types

---
+++ src/body.rs
@@ -0,0 +1,41 @@
+//! HTTP request body handling utilities.
+
+use {bytes::Bytes, std::future::Future, tower::BoxError};
+
+/// A trait for converting various body types into a [`Bytes`] object.
+///
+/// This requires reading the entire body into memory.
+pub trait IntoRequestBytes {
+    /// Convert this object into a [`Bytes`] object.
+    fn into_request_bytes(self) -> impl Future<Output = Result<Bytes, BoxError>> + Send + Sync;
+}
+
+/// Convert the unit type `()` into an empty [`Bytes`] object.
+impl IntoRequestBytes for () {
+    /// Convert the unit type `()` into an empty [`Bytes`] object.
+    ///
+    /// This is infalliable.
+    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
+        Ok(Bytes::new())
+    }
+}
+
+/// Convert a `Vec<u8>` into a [`Bytes`] object.
+impl IntoRequestBytes for Vec<u8> {
+    /// Convert a `Vec<u8>` into a [`Bytes`] object.
+    ///
+    /// This is infalliable.
+    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
+        Ok(Bytes::from(self))
+    }
+}
+
+/// Identity transformation: return the [`Bytes`] object as-is.
+impl IntoRequestBytes for Bytes {
+    /// Identity transformation: return the [`Bytes`] object as-is.
+    ///
+    /// This is infalliable.
+    async fn into_request_bytes(self) -> Result<Bytes, BoxError> {
+        Ok(self)
+    }
+}
